91b161fc56349ea24574018e67e3ecd125528bbd
mrAkshatJoshi <mrakshatjoshi@gmail.com>
2022-06-03 19:42:27 +0530

    first commit

---
+++ Toggling text/script.js
@@ -0,0 +1,8 @@
+function changeText(){
+    var x = document.getElementById("container")
+    if(x.innerHTML==="JavaScript"){
+        x.innerHTML="Hyper Text Markup Language"
+    }else{
+        x.innerHTML="JavaScript"
+    }
+}
