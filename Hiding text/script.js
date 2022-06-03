520a16b6c065bc04e447fd9c84e216055ca426d2
mrAkshatJoshi <mrakshatjoshi@gmail.com>
2022-06-03 20:45:00 +0530

    first commit

---
+++ Hiding text/script.js
@@ -0,0 +1,8 @@
+function hideBox(){
+    var x = document.getElementById("quote")
+    if(x.style.display==="none"){
+        x.style.display="block"
+    }else{
+        x.style.display="none"
+    }
+}
